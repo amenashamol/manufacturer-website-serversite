a3f02935f10468d9abd9c4e9d271a5c2c33cf006
amena <amena.ruet@gmail.com>
2022-05-28 11:39:28 +0600

    post

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require('express')
+const cors=require('cors')
+const { MongoClient, ServerApiVersion,  ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port=4000
 
 
-const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+//middlewire
+app.use(cors())
+app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tshjz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlf3n.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+  try{
+     
+      await client.connect()
+          const userCollection = client.db("manufacturer-company").collection("users");
+          
+      // get api to read all inventory 
+      app.get('/user',async(req,res)=>{
+          const query={}
+          const  cursor= userCollection.find(query)
+          const users= await cursor.toArray()
+         res.send(users) 
+      }) 
+      
+      
+      //create inventory item
+
+      app.post('/user',async(req,res)=>{
+          const data =req.body
+          const result=await userCollection.insertOne(data)
+          res.send(result)
+
+      })
+
+
+      //udate inventory
+
+      
+      
+   }
+
+   finally{
+
+   }
+
+}
+
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
