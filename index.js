f84932024a467591fb3384d54d793f1b2d7ea698
amena <amena.ruet@gmail.com>
2022-06-21 21:41:05 +0600

    verifyjwt

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlf3n.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run(){
   try{
@@ -34,7 +48,7 @@ async function run(){
 
     
 
-  app.get('/loginuser',async(req,res)=>{
+  app.get('/loginuser',verifyJWT,async(req,res)=>{
     
     const query={}
     const  cursor= loginCollection.find(query)
