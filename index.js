1a70089bcd56784e6ff3a22bd1f5fa2c2eb0696e
amena <amena.ruet@gmail.com>
2022-06-25 18:37:58 +0600

    booking

--- index.js
+++ index.js
@@ -195,7 +195,13 @@ async function run(){
       res.send(result) 
       })
       
-      
+      //booking 
+      app.get('/booking/:id', verifyJWT,async(req,res)=>{
+        const id=req.params.id 
+        const query={_id:ObjectId(id)}
+        const result= await ordersCollection.findOne(query)
+        res.send(result) 
+        })
       
    }
 
